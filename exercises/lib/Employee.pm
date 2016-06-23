ebae4f37afdec1e882fa02b10946f3d4d3939002
Robert Stone <drzigman@drzigman.com>
2016-06-23 09:08:31 -0500

    Exercise One
    In Person
      Made into Moose Object
      Added attributes: first_name, last_name
      BUILDARGS to allow calling with [ 'First', 'Last' ]
      Implemented: full_name
    
    In Employee
      Made into Moose Object
      Added attributes: title, salary, ssn
      Implemented: override of full_name

--- exercises/lib/Employee.pm
+++ exercises/lib/Employee.pm
@@ -1,4 +1,31 @@
 package Employee;
 
+use strict;
+use warnings;
+
+use Moose;
+use namespace::autoclean;
+
+extends 'Person';
+
+has 'title' => (
+    is => 'rw',
+);
+
+has 'salary' => (
+    is => 'rw',
+);
+
+has 'ssn' => (
+    is => 'ro',
+);
+
+override full_name => sub {
+    my $self = shift;
+
+    return sprintf("%s (%s)", super(), $self->title );
+};
+
+__PACKAGE__->meta->make_immutable;
 
 1;
--- exercises/lib/Person.pm
+++ exercises/lib/Person.pm
@@ -1,4 +1,43 @@
 package Person;
 
+use strict;
+use warnings;
+
+use Moose;
+use namespace::autoclean;
+
+use Data::Util qw( is_hash_ref is_array_ref );
+
+has 'first_name' => (
+    is => 'rw',
+);
+
+has 'last_name' => (
+    is => 'rw',
+);
+
+sub BUILDARGS {
+    my $class = shift;
+
+    # Allow calling with ->new( [ 'Lisa', 'Smith' ] );
+	if ( @_ == 1 && is_array_ref( $_[0] ) ) {
+        my $args = $_[0];
+
+	    return {
+            first_name => $args->[0],
+            last_name  => $args->[1],
+        };
+	}
+
+	return $class->SUPER::BUILDARGS( @_ );
+}
+
+sub full_name {
+    my $self = shift;
+
+    return $self->first_name . ' ' . $self->last_name;
+}
+
+__PACKAGE__->meta->make_immutable;
 
 1;
